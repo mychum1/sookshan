1fd65ae4a77f8320d3da2e970cf7e5c669db5e56
mychum1 <want5617@gmail.com>
2019-05-03 22:00:36 +0900

    initialize

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/test', function(req, res, next) {
+  console.log('hello world');
+  res.send('hello world');
+});
+
+
+module.exports = router;
