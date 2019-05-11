65b3a5a0ac79748a79eee1c41fd03fdaa66b6585
raehan <raehan809@elixirpay.com>
2019-05-11 16:19:24 +0900

    Create route for board

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/test', function(req, res, next) {
-  console.log('hello world');
-  res.send('hello world');
-});
-
-
 module.exports = router;
